2400688d8c0e11fc101fe1d6a14542508b5d1b57
Coder X <110594137+DoNotDistrub@users.noreply.github.com>
2022-09-11 14:30:56 +0500

    Initial commit

--- Asking user for radius andgiving area and circumfrence of circle .py
+++ Asking user for radius andgiving area and circumfrence of circle .py
@@ -1,7 +1,14 @@
+from uuid import RFC_4122
+
+
 R= float(input(" Please tell us radius : "))
 
-π = (3.1415)
+π= float(3.1415926535)
 
 print("area")
 
 print(π * (R*R))
+
+print("Circumfrence")
+
+print(2*π*R)
